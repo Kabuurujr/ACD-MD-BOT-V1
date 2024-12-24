4764f0ecbdf98fbd9ac2057141350dee2092315c
Kabuurujr <145644155+Kabuurujr@users.noreply.github.com>
2024-12-24 06:53:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94742398003']
+const ownerNumber = ['254796326254']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
